35f65de979af4284abce7915872132591dce7a25
Artvokerg <37901688+Artvokerg@users.noreply.github.com>
2018-03-29 15:29:16 +0300

    Add files via upload

---
+++ program/program4.java
@@ -0,0 +1,29 @@
+﻿
+package program4;
+
+import java.util.Scanner;
+
+
+public class program4 {
+
+    
+    public static void main(String[] args) {
+        int a, b, nod1, nok1;
+        Scanner in = new Scanner(System.in);
+        a = in.nextInt();
+        b = in.nextInt();
+        nod1 = nod(a, b);
+        System.out.println("НОД = " + nod1);
+        nok1 = nok(a, b);
+        System.out.println("НОK = " + nok1);
+    }
+    public static int nod(int a, int b){
+        if(b == 0)
+            return a;
+            else
+         return nod(b, a % b);
+    }
+    public static int nok(int a, int b){
+        return a * b / nod(a, b);
+    }
+}
