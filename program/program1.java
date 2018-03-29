ea082d9aa95f3fe1d77db8cb0f90555974a2255b
Artvokerg <37901688+Artvokerg@users.noreply.github.com>
2018-03-29 15:22:00 +0300

    Add files via upload

---
+++ program/program1.java
@@ -0,0 +1,27 @@
+﻿
+package program1;
+
+
+import java.util.Scanner;
+public class program1 {
+
+    public static void main(String[] args) {
+        int a, b;
+         Scanner in = new Scanner(System.in);
+         System.out.println("Введите 2 числа");
+         a = in.nextInt();
+         b = in.nextInt();
+        pol(a, b);
+    }
+    public static void pol(int a, int b){
+        for(int i = a; i<b; i++){
+            for(int k = 2; k<=i; k++){
+                if((i % k ==0) && (k!=i))
+                    break;
+                if(k == i)
+                  System.out.println(i);   
+            }
+        }
+            
+    }
+}
