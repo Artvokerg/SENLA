19a4b0f0f5202c1b06a1590953239de61a944780
Artvokerg <37901688+Artvokerg@users.noreply.github.com>
2018-03-29 15:27:33 +0300

    Add files via upload

---
+++ program/program2.java
@@ -0,0 +1,26 @@
+﻿
+package program2;
+
+import java.util.Scanner;
+public class program2 {
+
+    public static void main(String[] args) {
+        int n, b;
+        System.out.println("Введите число N");
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+        for(int i=1; i<n; i++){
+        b = los(i);
+        if(b > n)
+            break;
+        System.out.println(i + " = "+ b);
+        }
+    }
+    public static int los(int n){
+        if(n == 1 || n==2)
+            return 1;
+        else
+            return los(n-1)+los(n-2);
+    }
+    
+}
