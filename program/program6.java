8347c9afb88a3e39636965d751406af3d57ca386
Artvokerg <37901688+Artvokerg@users.noreply.github.com>
2018-03-29 15:31:56 +0300

    Add files via upload

---
+++ program/program6.java
@@ -0,0 +1,27 @@
+﻿
+package program6;
+
+
+import java.util.Scanner;
+public class program6 {
+    public static void main(String[] args) {
+        String s;
+        Scanner in = new Scanner(System.in);
+        s = in.nextLine();
+        System.out.println(s);
+        
+        
+       for(int i = 0; i<s.length(); i++){
+           char a=s.charAt(i);
+           if(a-48<=9 && a-48>=0){//по unicode
+            System.out.println(a);
+            String c = s.substring(i+1, s.length());
+            s = s.substring(0, i);
+            s = s.concat(c);
+            i--;
+           }
+       }
+        System.out.println(s);
+    }
+    
+}
